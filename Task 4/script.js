e8749f3681f41c5512cf7fad9a572226bda1c595
Rebeka777 <rebeka.renatosnamai@gmail.com>
2023-04-30 17:06:03 +0300

    4 task

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,36 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+document.addEventListener('DOMContentLoaded', async () => {
+  const ENDPOINT = 'cars.json';
+  const renderData = (brands) => {
+    const outputContainer = document.getElementById('output');
+    outputContainer.innerHTML = '';
+    brands.forEach((carData) => {
+      const car = document.createElement('div');
+      car.classList.add('car');
+      const brandName = document.createElement('span');
+      brandName.classList.add('brand-name');
+      brandName.textContent = carData.brand;
+      const modelsName = document.createElement('span');
+      modelsName.textContent = carData.models;
+      modelsName.classList.add('models-list');
+      car.append(brandName);
+      car.append(modelsName);
+      outputContainer.append(car);
+    });
+  };
+
+  const brands = async () => {
+    try {
+      const response = await fetch(ENDPOINT);
+      const data = await response.json();
+      if (response.ok) {
+        renderData(data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  await brands();
+});

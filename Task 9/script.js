430dfff35a4731cd521082f4c6e25ee05c824c9a
Rebeka777 <rebeka.renatosnamai@gmail.com>
2023-05-01 15:09:47 +0300

    9 task

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,15 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+const newMovie = new Movie('Life', 'Spielberg', 2000000000000);
+console.log(newMovie, newMovie.wasExpensive());
